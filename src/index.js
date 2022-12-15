9b4f22a347f6865a44c9b12635df1ad99d9cc3c8
Yegane <yeganehazratii@gmail.com>
2022-12-15 19:53:45 +0330

    chaging icon based on weather

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+function formatDate(timeStamp) {
+  let currentDate = new Date(timeStamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
+  let hour = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let date = document.querySelector("#date");
+  return `${day} ${hour}:${minutes}`;
+}
 function showTemp(response) {
   let tempC = Math.round(response.data.main.temp);
   let currentTempreture = document.querySelector("#current-temp");
@@ -10,6 +30,19 @@ function showTemp(response) {
   tempMax.innerHTML = `${maxTempC}°C`;
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#Humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  let realFeel = document.querySelector("#real-feel");
+  realFeel.innerHTML = Math.round(response.data.main.feels_like);
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   function tempToFar(event) {
     event.preventDefault();
     let tempToFarenheit = Math.round(response.data.main.temp * 1.8 + 32);
@@ -43,24 +76,3 @@ function searchEngine(event) {
 
 let searchButton = document.querySelector("#submit-form");
 searchButton.addEventListener("submit", searchEngine);
-
-let currentDate = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[currentDate.getDay()];
-let today = document.querySelector("#today");
-today.innerHTML = day;
-let hour = currentDate.getHours();
-let minutes = currentDate.getMinutes();
-let time = document.querySelector(".time");
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-time.innerHTML = `${hour}:${minutes}`;
